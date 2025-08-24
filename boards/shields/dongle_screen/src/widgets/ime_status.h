5b29799850f7c118a62bc93e9b4cdab84fb1e08a
YamadaSh10 <yamada.sh10.358@gmail.com>
2025-08-25 01:22:11 +0900

    ime_status.h修正

--- boards/shields/dongle_screen/src/widgets/ime_status.h
+++ boards/shields/dongle_screen/src/widgets/ime_status.h
@@ -21,6 +21,6 @@ struct zmk_widget_ime_status
 #define IME_STATUS_ON 1
 #define IME_STATUS_OFF 0
 
-int zmk_widget_ime_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent);
-lv_obj_t *zmk_widget_ime_status_obj(struct zmk_widget_wpm_status *widget);
+int zmk_widget_ime_status_init(struct zmk_widget_ime_status *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_ime_status_obj(struct zmk_widget_ime_status *widget);
 extern void ime_status_update(ime_status_state state);
