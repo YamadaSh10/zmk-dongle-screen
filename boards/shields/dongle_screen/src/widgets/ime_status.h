60b6d6d3fd80d1585b1d856b983af497159eeadb
YamadaSh10 <yamada.sh10.358@gmail.com>
2025-08-30 01:45:21 +0900

    ドット絵サイズ変更

--- boards/shields/dongle_screen/src/widgets/ime_status.h
+++ boards/shields/dongle_screen/src/widgets/ime_status.h
@@ -21,15 +21,18 @@ struct zmk_widget_ime_status
 #define IME_STATUS_ON 1
 #define IME_STATUS_OFF 0
 
-//#define DOT_COLOR_WHITE 0xFFFFFF, 0xFFFFFF
-#define DOT_COLOR_WHITE 0x000000, 0x000000
-#define DOT_COLOR_BLACK 0x000000, 0x000000
-#define DOT_COLOR_RED0 0xE00000, 0xE00000
-#define DOT_COLOR_RED1 0xA70503, 0xA70503
-#define DOT_COLOR_YELLOW0 0xF6F76B, 0xF6F76B
-#define DOT_COLOR_YELLOW1 0xF5E700, 0xF5E700
-#define DOT_COLOR_YELLOW2 0xE5B400, 0xE5B400
-#define DOT_COLOR_YELLOW3 0xBA7E00, 0xBA7E00
+#define DOT_COLOR_WHITE 0xFFFFFF
+#define DOT_COLOR_BLACK 0x000000
+#define DOT_COLOR_RED0 0xE00000
+#define DOT_COLOR_RED1 0xA70503
+#define DOT_COLOR_YELLOW0 0xF6F76B
+#define DOT_COLOR_YELLOW1 0xF5E700
+#define DOT_COLOR_YELLOW2 0xE5B400
+#define DOT_COLOR_YELLOW3 0xBA7E00
+#define DOT_COLOR_BACK DOT_COLOR_BLACK
+
+#define ILLUST_WIDTH 45
+#define ILLUST_HEIGHT 30
 
 int zmk_widget_ime_status_init(struct zmk_widget_ime_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_ime_status_obj(struct zmk_widget_ime_status *widget);
