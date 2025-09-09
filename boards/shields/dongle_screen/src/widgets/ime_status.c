9bab83e2cec136b175424fc19865d38e4485b90b
YamadaSh10 <yamada.sh10.358@gmail.com>
2025-09-10 02:06:45 +0900

    スリープ時の絵を変更

--- boards/shields/dongle_screen/src/widgets/ime_status.c
+++ boards/shields/dongle_screen/src/widgets/ime_status.c
@@ -82,7 +82,7 @@ static uint32_t illust_color_code[ILLUST_WIDTH * ILLUST_HEIGHT] =
 };
 #endif
 static lv_color_t buffer[LV_CANVAS_BUF_SIZE_TRUE_COLOR(ILLUST_WIDTH, ILLUST_HEIGHT)];
-const lv_img_dsc_t blue_ham_ham_mini_left = {
+/* const lv_img_dsc_t blue_ham_ham_mini_left = {
   .header.cf = LV_IMG_CF_INDEXED_2BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
@@ -99,6 +99,16 @@ const lv_img_dsc_t blue_ham_ham_mini_right = {
   .header.h = 90,
   .data_size = 1816,
   .data = blue_ham_ham_mini_right_map,
+}; */
+
+const lv_img_dsc_t mentako_ya = {
+  .header.cf = LV_IMG_CF_ALPHA_1BIT,
+  .header.always_zero = 0,
+  .header.reserved = 0,
+  .header.w = 90,
+  .header.h = 90,
+  .data_size = 1080,
+  .data = mentako_ya_map,
 };
 
 
@@ -127,14 +137,15 @@ int zmk_widget_ime_status_init(struct zmk_widget_ime_status *widget, lv_obj_t *p
     lv_obj_set_size(widget->obj, 280, 240);
 
     lv_obj_t *image_canvas = lv_canvas_create(widget->obj);
-    lv_canvas_set_buffer(image_canvas, buffer, ILLUST_WIDTH*2, ILLUST_HEIGHT, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_set_buffer(image_canvas, buffer, ILLUST_WIDTH, ILLUST_HEIGHT, LV_IMG_CF_TRUE_COLOR);
 
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
     img_dsc.opa = LV_OPA_COVER;  // 不透明度100%
 
-    lv_canvas_draw_img(image_canvas, 0, 0, &blue_ham_ham_mini_left, &img_dsc);
-    lv_canvas_draw_img(image_canvas, ILLUST_WIDTH, 0, &blue_ham_ham_mini_right, &img_dsc);
+    /* lv_canvas_draw_img(image_canvas, 0, 0, &blue_ham_ham_mini_left, &img_dsc);
+    lv_canvas_draw_img(image_canvas, ILLUST_WIDTH, 0, &blue_ham_ham_mini_right, &img_dsc); */
+    lv_canvas_draw_img(image_canvas, 0, 0, &mentako_ya, &img_dsc);
     lv_obj_align(image_canvas, LV_ALIGN_CENTER, 0, 0);
     /*
     for(int i=0; i<ILLUST_WIDTH; i++)
